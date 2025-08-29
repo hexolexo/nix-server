e49411855cb257fc3b954a4f8e63384b7b39efe3
hexolexo <hexolexo132@proton.me>
2025-08-29 20:47:19 +1000

    maybe?

--- containers/wireguard.nix
+++ containers/wireguard.nix
@@ -1,12 +1,4 @@
 {...}: {
-  #containers.wireguard = {
-  #autoStart = true;
-  #privateNetwork = false;
-  #config = {
-  #pkgs,
-  #lib,
-  #...
-  #}: {
   networking.wireguard.interfaces.wg0 = {
     privateKeyFile = "/etc/wireguard/private";
     listenPort = 51820;
@@ -23,6 +15,7 @@
       }
       {
         publicKey = "v54b/A7ynLrcXBMcgJkf6vgzJgra8Z3BkaFHMy1RMWk="; # death
+        persistentKeepalive = 25;
         allowedIPs = ["10.0.0.4/32"];
       }
       {
@@ -36,9 +29,4 @@
     ];
   };
   networking.firewall.allowedUDPPorts = [51820];
-
-  #boot.isContainer = true;
-  #system.stateVersion = "25.05";
-  #};
-  #};
 }

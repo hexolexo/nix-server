c2931dc085063607335b4a9e1c9306d5a28b3e16
hexolexo <hexolexo132@proton.me>
2025-08-29 17:33:15 +1000

    I should never have been allowed to run a server

--- containers/wireguard.nix
+++ containers/wireguard.nix
@@ -10,11 +10,27 @@
       networking.wireguard.interfaces.wg0 = {
         privateKeyFile = "/etc/wireguard/private";
         listenPort = 51820;
-        ips = ["10.0.0.1/24" "192.168.1.108/32"];
+        ips = ["10.0.0.1/24"];
         peers = [
           {
-            publicKey = "vWCeMXGBA2v5bV+kX/otvPi/+v9DSAzKnrBqqbbB31k=";
+            publicKey = "vWCeMXGBA2v5bV+kX/otvPi/+v9DSAzKnrBqqbbB31k="; # hexolexo
             allowedIPs = ["10.0.0.2/32"];
+          }
+          {
+            publicKey = "LIOuTaH9hb6tVrQHADclTFnCcRFD9DHjaVbNSevfGxc="; # toshi
+            allowedIPs = ["10.0.0.3/32"];
+          }
+          {
+            publicKey = "v54b/A7ynLrcXBMcgJkf6vgzJgra8Z3BkaFHMy1RMWk="; # death
+            allowedIPs = ["10.0.0.4/32"];
+          }
+          {
+            publicKey = "KsWkLxtO3pbgeolMGpF1PhuMTg9vfcZ2GuPUyoYT6Xg="; # Euroacorn
+            allowedIPs = ["10.0.0.5/32"];
+          }
+          {
+            publicKey = "ePTxrL7Fgqdl6HO/ZImMHMLnXBysGQ8+h0t+6pBJiRE="; # guest
+            allowedIPs = ["10.0.0.6/32"];
           }
         ];
       };

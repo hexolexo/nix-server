72299693105a7c2b2b9f7e1b9ae1858f27abda26
hexolexo <hexolexo132@proton.me>
2025-11-07 19:34:41 +1100

    Maybe murmur might suck less

---
+++ containers/murmur.nix
@@ -0,0 +1,19 @@
+{...}: {
+  containers.jitsi = {
+    autoStart = true;
+    config = {
+      pkgs,
+      lib,
+      ...
+    }: {
+      services.murmur = {
+        enable = true;
+        openFirewall = true;
+        #password = "your-server-password"; # Optional server password
+      };
+      system.stateVersion = "25.05";
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [8096];
+}
